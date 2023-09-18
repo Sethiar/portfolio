162c8793e0f9cd71e1521a7eb0f819832143e3ef
Lefetey Arnaud <alefetey123@gmail.com>
2023-09-18 19:41:20 +0200

    Mise à jour main.js

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,32 +1,29 @@
+
 import "movements_navette/movements_navette.js";
 import "elements_interface/target_missile.js";
+import "elements_interface/message.js"
 import "FX/explosions.js";
-
-document.addEventListener("DOMContentLoaded", function () {
-  // Code principal restant
-});
 
 document.addEventListener("DOMContentLoaded", function () {
   // Fonction pour animer le défilement de l'arrière-plan
   function moveBackgroundDown() {
-    const background = document.querySelector(".background");
-    const windowHeight = window.innerHeight;
+     const background = document.getElementById("background");
 
-    // Définissez l'animation CSS en fonction de la hauteur de la fenêtre
-    document.styleSheets[0].insertRule(`@keyframes moveBackgroundDown {
-      0% {
-        background-position-y: 0;
-      }
-      100% {
-        background-position-y: ${windowHeight}px;
-      }
-    }`, 0);
+    // Mettez à jour la hauteur de l'animation CSS à chaque redimensionnement de fenêtre
+     function updateAnimationHeight() {
+      const windowHeight = window.innerHeight;
+      background.style.animationDuration = "1s";
+      background.style.animationTimingFunction = "linear";
+      background.style.animationIterationCount = "infinite";
+      background.style.animationPlayState = "running"; // Réinitialisez l'animation
+    }
 
-    // Appliquez l'animation à l'élément .background
-    background.style.animationName = "moveBackgroundDown";
-    background.style.animationDuration = "10s";
-    background.style.animationTimingFunction = "linear";
-    background.style.animationIterationCount = "infinite";
+
+    // Appelez la fonction pour mettre à jour la hauteur de l'animation lors du chargement initial
+    updateAnimationHeight();
+
+    // Ajoutez un gestionnaire d'événement pour le redimensionnement de la fenêtre
+    window.addEventListener("resize", updateAnimationHeight);
   }
 
   // Appelez la fonction pour initialiser l'animation de l'arrière-plan
