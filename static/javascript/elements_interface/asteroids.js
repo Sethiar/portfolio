d3c7c94ddc0fc2611512773c6f828c531ce23ecd
Lefetey Arnaud <alefetey123@gmail.com>
2023-09-19 14:52:37 +0200

    Ajout de  asteroids.js

--- static/javascript/elements_interface/asteroids.js
+++ static/javascript/elements_interface/asteroids.js
@@ -1,52 +1,54 @@
-
-// Fonction pour générer aléatoirement un astéroïde
-function generateAsteroid() {
+document.addEventListener("DOMContentLoaded", function () {
+  function createAsteroid(className, linkId) {
     const asteroid = document.createElement('div');
-    asteroid.className = 'asteroid';
+    asteroid.className = className;
 
-
-    // Position aléatoire en haut de la page
     const randomX = Math.random() * window.innerWidth;
     asteroid.style.left = randomX + 'px';
-    asteroid.style.top = '-20px'; // Commence en haut de la page
+    asteroid.style.top = '-20px';
     document.body.appendChild(asteroid);
+    asteroid.style.display = 'block';
 
-    // Animer la chute de l'astéroïde
-    const fallSpeed = Math.random() * 4 + 2; // Vitesse de chute aléatoire
+    const fallSpeed = Math.random() * 4 + 2;
     const intervalId = setInterval(() => {
-        const currentTop = parseInt(asteroid.style.top, 10);
-        asteroid.style.top = (currentTop + fallSpeed) + 'px';
+      const currentTop = parseInt(asteroid.style.top, 10);
+      asteroid.style.top = (currentTop + fallSpeed) + 'px';
 
-        // Si l'astéroïde atteint le bas de la page, le supprimer
-        if (currentTop > window.innerHeight) {
-                    clearInterval(intervalId);
-                    asteroid.remove();
+      if (currentTop > window.innerHeight) {
+        clearInterval(intervalId);
+        asteroid.remove();
+      }
 
-        }
-
-        // Vérifiez la collision entre l'astéroïde et le missile à chaque mise à jour de position
-        detecterCollisionAsteroid(asteroid);
-    // Rafraîchissement toutes les 50 ms
+      detectCollision(asteroid, linkId);
     }, 50);
-}
-
-// Générez aléatoirement des astéroïdes toutes les quelques secondes
-setInterval(generateAsteroid, 6000); // par exemple, toutes les 6 secondes
-
-function detecterCollisionAsteroid(asteroid) {
-  const missile = document.getElementById("missile");
-  const rectAsteroid = asteroid.getBoundingClientRect();
-  const rectMissile = missile.getBoundingClientRect();
-
-  if (
-    rectMissile.left < rectAsteroid.right &&
-    rectMissile.right > rectAsteroid.left &&
-    rectMissile.top < rectAsteroid.bottom &&
-    rectMissile.bottom > rectAsteroid.top
-  ) {
-    // Collision détectée entre l'astéroïde et le missile
-    // Supprime l'astéroïde
-    asteroid.remove();
-    missile.style.display = "none";
   }
-}
+
+  function detectCollision(asteroid, linkId) {
+    const missile = document.getElementById("missile");
+    const rectAsteroid = asteroid.getBoundingClientRect();
+    const rectMissile = missile.getBoundingClientRect();
+
+    if (
+      rectMissile.left < rectAsteroid.right &&
+      rectMissile.right > rectAsteroid.left &&
+      rectMissile.top < rectAsteroid.bottom &&
+      rectMissile.bottom > rectAsteroid.top
+    ) {
+      asteroid.remove();
+      missile.style.display = "none";
+      document.getElementById(linkId).style.display = "inline";
+    }
+  }
+
+  setInterval(() => {
+    createAsteroid('asteroid1', 'cv-link');
+  }, 10000);
+
+  setInterval(() => {
+    createAsteroid('asteroid2', 'projets-link');
+  }, 7000);
+
+  setInterval(() => {
+    createAsteroid('asteroid3', 'competences-link');
+  }, 6000);
+});
