ece0edf0ae08d2656d0e8980a0d9b43972a45f05
Lefetey Arnaud <alefetey123@gmail.com>
2023-09-20 23:18:18 +0200

    Ajout redirection_cv.js

---
+++ static/javascript/elements_cv/redirection_cv.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const earth = document.getElementById("animated_earth");
+    const navette = document.getElementById("navette");
+
+    // Fonction pour gérer le clic sur la souris
+    earth.addEventListener("click", function () {
+        window.location.href = ""; // Remplacez par le lien vers le document de la Terre
+    });
+
+    // Fonction pour gérer l'appui sur la touche Entrée
+    document.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            const earthRect = earth.getBoundingClientRect();
+            const navetteRect = navette.getBoundingClientRect();
+
+            // Vérifiez si la souris est positionnée sur la Terre
+            if (
+                (event.target === earth || event.target === navette) &&
+                (
+                    (navetteRect.left >= earthRect.left &&
+                    navetteRect.right <= earthRect.right &&
+                    navetteRect.top >= earthRect.top &&
+                    navetteRect.bottom <= earthRect.bottom)
+                    ||
+                    (event.target === earth)
+                )
+            ) {
+                window.location.href = ""; // Redirection vers le document de la Terre
+            }
+        }
+    });
+});
