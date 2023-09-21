130266a336645146da8a64ffa154458027f59b0f
Lefetey Arnaud <alefetey123@gmail.com>
2023-09-21 11:20:15 +0200

    Ajout fonctionnalité cookies dans app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Fichier app.py contenant toutes les routes de mon portfolio"""
 
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 
 import os
 import locale
@@ -14,6 +14,33 @@ app = Flask("Portfolio", template_folder="templates")
 locale.setlocale(locale.LC_TIME, 'fr_FR.utf8')
 
 
+@app.route("/consentement", methods=["POST"])
+def consentement():
+    consent_data = request.json
+    if consent_data["consent"] == "accepted":
+        # Faites ce que vous voulez en cas d'acceptation
+        return "/home"
+
+    elif consent_data["consent"] == "rejected":
+        # Faites ce que vous voulez en cas de refus
+        return "/refus-cookie"
+
+    else:
+        # Traitement en cas de données de consentement inconnues ou incorrectes
+        return "Données de consentement invalides"
+
+
+@app.route("/")
+def modale():
+    return render_template("modale.html")
+
+
+@app.route("/refus-cookie")
+def refus_cookie():
+    return render_template("refus-cookie.html")
+
+
+
 # Route vers la page 404 de mon site
 @app.route('/404')
 def page404():
@@ -24,7 +51,7 @@ Access to 404 page.
     return render_template("404.html")
 
 # Ma page d'accueil
-@app.route('/')
+@app.route('/home')
 def home():
     return render_template("home.html")
 
@@ -44,9 +71,27 @@ def cv():
     return send_from_directory("static/pdf", "CV_arnaud.pdf")
 
 
-# Les projets personnels que je présente dans mon portfolio
+@app.route('/home/M1')
+def notes_m1():
+    return send_from_directory("static/pdf", "Relevé_note_M1.pdf")
+
+
+@app.route('/home/attestation-reussite')
+def attestation_reussite():
+    return send_from_directory("static/pdf", "Attestation_de_reussite.pdf")
+
+
+@app.route('/home/Resultats-formation')
+def resultats_formation():
+    return send_from_directory("static/pdf", "Resultats_Formation.pdf")
+
+
 @app.route('/home/projets')
 def projet_perso():
+    """
+Les projets personnels que je présente dans mon portfolio
+    :return:
+    """
     return render_template("projets.html")
 
 
@@ -54,6 +99,30 @@ def projet_perso():
 @app.route('/home/competences')
 def competences():
     return render_template("competences.html")
+
+
+@app.route("/conditions-utilisation")
+def condition():
+    return render_template("conditions.html")
+
+
+@app.route("/politique-confidentialité")
+def politique():
+    return render_template("politique.html")
+
+
+
+
+@app.route("/sitemap.xml")
+def sitemap():
+    return send_from_directory(".", "sitemap.xml")
+
+
+@app.route("/robots.txt")
+def robots():
+    return send_from_directory(".", "robots.txt")
+
+
 
 
 port = int(os.environ.get("PORT", 5000))
---
+++ static/javascript/modale.js
@@ -0,0 +1,55 @@
+// Fonction pour définir un cookie
+function setCookie(name, value, days, secure = true) {
+    var expires = new Date();
+    expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+
+    // Utilisation de la propriété "secure" pour définir si le cookie doit être sécurisé ou non
+    var cookieString = name + '=' + value + ';expires=' + expires.toUTCString();
+    if (secure) {
+        cookieString += ';secure;SameSite=Strict';
+    }
+    document.cookie = cookieString;
+}
+
+// Attente du chargement complet du document
+document.addEventListener("DOMContentLoaded", function () {
+    var modale = document.getElementById('fenetre_modale');
+    var acceptBtn = document.getElementById('acceptBtn');
+    var rejectBtn = document.getElementById('rejectBtn');
+
+    // Afficher la modale
+    modale.style.display = 'block';
+
+    // Gérer les clics sur les boutons
+    acceptBtn.addEventListener('click', function() {
+        setCookie('cookie_consent', 'accepted', 30);
+        sendConsentToServer('accepted');  // Envoie les données au serveur
+        modale.style.display = 'none';
+        console.log("Accept button clicked");
+    });
+
+    rejectBtn.addEventListener('click', function() {
+        setCookie('cookie_consent', 'rejected', 30);
+        sendConsentToServer('rejected');  // Envoie les données au serveur
+        modale.style.display = 'none';
+        console.log("refuse button clicked");
+    });
+
+    function sendConsentToServer(consent) {
+        fetch("/consentement", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ consent: consent })
+        })
+        .then(response => response.text())
+        .then(data => {
+            console.log("Response from server:", data);
+            window.location.href = data;
+        })
+        .catch(error => {
+            console.error('Erreur lors de l\'envoi des données :', error);
+        });
+    }
+});
