24d524d1b4cae826fd939ed196a4a91d7717ae41
Lefetey Arnaud <alefetey123@gmail.com>
2023-09-21 11:26:11 +0200

    Mal redirection.js

--- static/javascript/elements_cv/redirection_cv.js
+++ static/javascript/elements_cv/redirection_cv.js
@@ -1,32 +1,48 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const earth = document.getElementById("animated_earth");
-    const navette = document.getElementById("navette");
+    const spaceship = document.querySelector(".spaceship");
 
-    // Fonction pour gérer le clic sur la souris
-    earth.addEventListener("click", function () {
-        window.location.href = "/home/cv"; // Remplacez par le lien vers le document cv
-    });
+    // Fonction de gestion du clic, de l'appui sur Entrée, et du surlignage
+    function handlePlanetInteraction(planet, destination) {
+        planet.addEventListener("click", function () {
+            window.location.href = destination;
+        });
 
-    // Fonction pour gérer l'appui sur la touche Entrée
-    document.addEventListener("keydown", function (event) {
-        if (event.key === "Enter") {
-            const earthRect = earth.getBoundingClientRect();
-            const navetteRect = navette.getBoundingClientRect();
+        document.addEventListener("keydown", function (event) {
+            if (event.key === "Enter") {
+                const planetRect = planet.getBoundingClientRect();
+                const spaceshipRect = spaceship.getBoundingClientRect();
 
-            // Vérifiez si la souris est positionnée sur la Terre
-            if (
-                (event.target === earth || event.target === navette) &&
-                (
-                    (navetteRect.left >= earthRect.left &&
-                    navetteRect.right <= earthRect.right &&
-                    navetteRect.top >= earthRect.top &&
-                    navetteRect.bottom <= earthRect.bottom)
-                    ||
-                    (event.target === earth)
-                )
-            ) {
-                window.location.href = "/home/cv"; // Redirection vers le document cv
+                const spaceshipX = spaceshipRect.left + spaceshipRect.width / 2;
+                const spaceshipY = spaceshipRect.top + spaceshipRect.height / 2;
+
+                const planetX = planetRect.left + planetRect.width / 2;
+                const planetY = planetRect.top + planetRect.height / 2;
+
+                const distance = Math.sqrt((planetX - spaceshipX) ** 2 + (planetY - spaceshipY) ** 2);
+
+                if (distance <= (planetRect.width / 2)) {
+                    window.location.href = destination;
+                }
             }
-        }
-    });
+        });
+
+        planet.addEventListener("mouseover", function () {
+            planet.style.boxShadow = "0 0 10px gold";
+        });
+
+        planet.addEventListener("mouseout", function () {
+            planet.style.boxShadow = "none";
+        });
+    }
+
+    const earth = document.getElementById("animated_earth");
+    const mars = document.getElementById("animated_mars");
+    const planet1 = document.getElementById("planet1");
+    const planet2 = document.getElementById("planet2");
+
+    // Appel de la fonction pour chaque planète avec sa destination respective
+    handlePlanetInteraction(earth, "/home/cv");
+    handlePlanetInteraction(mars, "/home/M1");
+    handlePlanetInteraction(planet1, "/home/attestation-reussite");
+    handlePlanetInteraction(planet2, "/home/Resultats-formation");
 });
