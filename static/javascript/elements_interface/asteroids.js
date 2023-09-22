461bae69e266197f55889f6655a2942f1085f6b1
Lefetey Arnaud <alefetey123@gmail.com>
2023-09-22 14:34:00 +0200

    Maj asteroids.js

--- static/javascript/elements_interface/asteroids.js
+++ static/javascript/elements_interface/asteroids.js
@@ -1,26 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
+  let asteroidCount = 0;
+  const maxAsteroids = 12; // Nombre maximal d'astéroïdes
+
   function createAsteroid(className, linkId) {
-    const asteroid = document.createElement('div');
-    asteroid.className = className;
+    if (asteroidCount < maxAsteroids) { // Vérifie la limite
+      const asteroid = document.createElement('div');
+      asteroid.className = className;
 
-    const randomX = Math.random() * window.innerWidth;
-    asteroid.style.left = randomX + 'px';
-    asteroid.style.top = '-20px';
-    document.body.appendChild(asteroid);
-    asteroid.style.display = 'block';
+      const randomX = Math.random() * window.innerWidth;
+      asteroid.style.left = randomX + 'px';
+      asteroid.style.top = '-20px';
+      document.body.appendChild(asteroid);
+      asteroid.style.display = 'block';
 
-    const fallSpeed = Math.random() * 4 + 2;
-    const intervalId = setInterval(() => {
-      const currentTop = parseInt(asteroid.style.top, 10);
-      asteroid.style.top = (currentTop + fallSpeed) + 'px';
+      const fallSpeed = Math.random() * 4 + 2;
+      const intervalId = setInterval(() => {
+        const currentTop = parseInt(asteroid.style.top, 10);
+        asteroid.style.top = (currentTop + fallSpeed) + 'px';
 
-      if (currentTop > window.innerHeight) {
-        clearInterval(intervalId);
-        asteroid.remove();
-      }
+        if (currentTop > window.innerHeight) {
+          clearInterval(intervalId);
+          asteroid.remove();
+          asteroidCount--;
+        }
 
-      detectCollision(asteroid, linkId);
-    }, 50);
+        detectCollision(asteroid, linkId);
+      }, 50);
+
+      asteroidCount++; // Incrémentation du compteur
+    }
   }
 
   function detectCollision(asteroid, linkId) {
@@ -42,7 +50,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   setInterval(() => {
     createAsteroid('asteroid1', 'cv-link');
-  }, 10000);
+  }, 5000);
 
   setInterval(() => {
     createAsteroid('asteroid2', 'projets-link');
