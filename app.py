2bd30a090338e27e5b9184cdf508d6cfc7cf61ef
Lefetey Arnaud <alefetey123@gmail.com>
2023-09-30 23:22:06 +0200

    Maj version anglaise du site

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ Création de la fonction de création de mon application
     Access to 404 page.
         :return:
         """
-        return render_template("404.html", assets=assets)
+        return render_template("error404.html", assets=assets)
 
     # Ma page d'accueil
     @app.route('/home')
@@ -117,7 +117,17 @@ Création de la fonction de création de mon application
         """
         return render_template("home.html", assets=assets)
 
-    # Mon curriculum vitae que je présente dans mon portfolio
+    # Version anglaise : Ma page d'accueil
+    @app.route('/home/eng')
+    def homeeng():
+        """
+    Route menant à la verison anglaise de l'accueil du portfolio
+        :return:
+        """
+        return render_template("homeeng.html", assets=assets)
+
+
+# Mon curriculum vitae que je présente dans mon portfolio
     @app.route('/home/cv_access')
     def cv_access():
         """
@@ -146,6 +156,36 @@ Création de la fonction de création de mon application
         else:
             return "Erreur lors de la récupération des données lunaires."
 
+    # Version anglaise : Mon curriculum vitae que je présente dans mon portfolio
+    @app.route('/home/cv_access/eng')
+    def cv_accesseng():
+        """
+    Route donnant accès à la version anglaise de la section personnelle du portfolio
+        :return:
+        """
+        current_datetime = datetime.datetime.now()
+
+        # Display the current date
+        current_date = current_datetime.strftime('%A, %B %d, %Y')
+
+        # Display the current time
+        hour = current_datetime.strftime('%H:%M')
+
+        # Build the URL with the current date
+        api_url = "https://www.icalendar37.net/lunar/api/?lang=en&month={}&year={}&size=100&lightColor=rgb(245,245,245)&shadeColor=rgb(17,17,17)&LDZ={}".format(
+            current_datetime.month,
+            current_datetime.year,
+            int(current_datetime.timestamp())
+        )
+
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            data = response.json()
+            return render_template("cveng.html", hour=hour, current_date=current_date, moon_data=data, assets=assets)
+        else:
+            return "Error occurred while retrieving lunar data."
+
+    # Présentation de mon cv
     @app.route('/home/cv')
     def cv():
         """
@@ -155,6 +195,17 @@ Création de la fonction de création de mon application
 
         return send_from_directory("static/pdf", "CV_arnaud.pdf")
 
+    # Version anglaise : Présentation de mon cv
+    @app.route('/home/cv/eng')
+    def cveng():
+        """
+    Route affichant le curriculum vitae dans la version anglaise
+        :return:
+        """
+
+        return send_from_directory("static/pdf", "CV_arnaud_eng.pdf")
+
+    # Présentation Notes de M1
     @app.route('/home/M1')
     def notes_m1():
         """
@@ -163,6 +214,7 @@ Création de la fonction de création de mon application
         """
         return send_from_directory("static/pdf", "Relevé_note_M1.pdf")
 
+    # Attestation de réussite
     @app.route('/home/attestation-reussite')
     def attestation_reussite():
         """
@@ -171,6 +223,7 @@ Création de la fonction de création de mon application
         """
         return send_from_directory("static/pdf", "Attestation_de_reussite.pdf")
 
+    # Résultats de la formation
     @app.route('/home/resultats-formation')
     def resultats_formation():
         """
@@ -179,6 +232,7 @@ Création de la fonction de création de mon application
         """
         return send_from_directory("static/pdf", "resultats_Formation.pdf")
 
+    # Présentation des projets
     @app.route('/home/projets')
     def projet_perso():
         """
@@ -186,6 +240,15 @@ Création de la fonction de création de mon application
         :return:
         """
         return render_template("projets.html", assets=assets)
+
+    # Version anglaise : Présentation des projets
+    @app.route('/home/projects-section')
+    def projects_section():
+        """
+    Les projets personnels que je présente dans la version anglaise de mon portfolio
+        :return:
+        """
+        return render_template("projects_section.html", assets=assets)
 
     # Les compétences que je présente dans mon portfolio
     @app.route('/home/competences')
@@ -196,6 +259,16 @@ Création de la fonction de création de mon application
         """
         return render_template("competences.html", assets=assets)
 
+    # Version anglaise : Les compétences que je présente dans mon portfolio
+    @app.route('/home/skill-section')
+    def skill_section():
+        """
+    Route permettant d'accéder à mes compétences
+        :return:
+        """
+        return render_template("skill_section.html", assets=assets)
+
+    # Accès aux conditions d'utilisation de mon portfolio
     @app.route("/conditions-utilisation")
     def condition():
         """
@@ -204,13 +277,50 @@ Création de la fonction de création de mon application
         """
         return render_template("conditions.html", assets=assets)
 
+    # Version anglaise : Accès aux conditions d'utilisation de mon portfolio
+    @app.route("/terms-of-use")
+    def terms_of_use():
+        """
+    Route affichant les conditions d'utilisation de la version anglaise du site
+        :return:
+        """
+        return render_template("terms_of_use.html", assets=assets)
+
+    # Accès à la politique de confidentialité de mon portfolio
     @app.route("/politique-confidentialité")
     def politique():
         """
-    Rute affichant la politique d'utilisation du site
+    Route affichant la politique d'utilisation du site
         :return:
         """
         return render_template("politique.html", assets=assets)
+
+    # Version anglaise : Accès à la politique de confidentialité de mon portfolio
+    @app.route("/privacy-policy")
+    def privacy_policy():
+        """
+    Route affichant la politique d'utilisation de la version anglmaise du site
+        :return:
+        """
+        return render_template("privacy_policy.html", assets=assets)
+
+    # Page de remerciements aux contributeurs
+    @app.route("/remerciements")
+    def merci():
+        """
+    Route affichant la page de remerciements aux auteurs des oeuvres utilisées sur ce site
+        :return:
+        """
+        return render_template("remerciements.html", assets=assets)
+
+    # Version anglaise : Page de remerciements aux contributeurs
+    @app.route("/acknowledgements")
+    def acknowledgements():
+        """
+    Route affichant la page de remerciements version anglaise aux auteurs des oeuvres utilisées pour le  site
+        :return:
+        """
+        return render_template("Acknowledgements.html", assets=assets)
 
     @app.route("/sitemap.xml")
     def sitemap():
